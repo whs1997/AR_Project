16d0a50bad9e24d19a0063aa2c37b358148dfd91
whs1997 <whs1997@naver.com>
2024-10-08 19:58:01 +0900

    [FEAT] 이미지트래킹 오브젝트 끄기
    트래킹 도중 물체가 없어지거나 찾지 못하면 몇초 뒤 프리팹 사라지게 함

--- Assets/ImageTracker.cs
+++ Assets/ImageTracker.cs
@@ -1,56 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
 public class ImageTracker : MonoBehaviour
 {
-    public float timer;
-
-    [SerializeField] ARTrackedImageManager imageManager;
-
-    [SerializeField] List<GameObject> objectList = new List<GameObject>();
-    private Dictionary<string, GameObject> prefabDic = new Dictionary<string, GameObject>();
-
+    public float _timer;
+    public ARTrackedImageManager trackedImageManager;
+    public List<GameObject> _objectList = new List<GameObject>();
+    private Dictionary<string, GameObject> _prefabDic = new Dictionary<string, GameObject>();
     private List<ARTrackedImage> _trackedImg = new List<ARTrackedImage>();
     private List<float> _trackedTimer = new List<float>();
 
     private void Awake()
     {
-        foreach(GameObject obj in objectList)
+        foreach (GameObject obj in _objectList)
         {
-            string Name = obj.name;
-            prefabDic.Add(Name, obj);
-        }
+            string tName = obj.name;
+
+            _prefabDic.Add(tName, obj);
+        } 
     }
 
     private void Update()
     {
-        if(_trackedImg.Count > 0)
+        if (_trackedImg.Count > 0)
         {
             List<ARTrackedImage> tNumList = new List<ARTrackedImage>();
-
-            for(int i=0; i<_trackedImg.Count; i++)
+            for (var i = 0; i < _trackedImg.Count; i++)
             {
                 if (_trackedImg[i].trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Limited)
                 {
-                    if (_trackedTimer[i] > timer)
+                    if (_trackedTimer[i] > _timer)
                     {
                         string name = _trackedImg[i].referenceImage.name;
-                        GameObject tObj = prefabDic[name];
+                        GameObject tObj = _prefabDic[name];
                         tObj.SetActive(false);
                         tNumList.Add(_trackedImg[i]);
                     }
                     else
                     {
                         _trackedTimer[i] += Time.deltaTime;
+                        Debug.Log($"timer {_timer}, _trackedTimer {_trackedTimer[i]}");
                     }
                 }
             }
-
+            
             if (tNumList.Count > 0)
             {
-                for(int i=0; i<tNumList.Count; i++)
+                for (var i = 0; i < tNumList.Count; i++)
                 {
                     int num = _trackedImg.IndexOf(tNumList[i]);
                     _trackedImg.Remove(_trackedImg[num]);
@@ -59,20 +58,20 @@ public class ImageTracker : MonoBehaviour
             }
         }
     }
-
+    
     private void OnEnable()
     {
-        imageManager.trackedImagesChanged += ImageChanged;
+        trackedImageManager.trackedImagesChanged += ImageChanged;
     }
 
     private void OnDisable()
     {
-        imageManager.trackedImagesChanged -= ImageChanged;
+        trackedImageManager.trackedImagesChanged -= ImageChanged;
     }
-
-    private void ImageChanged(ARTrackedImagesChangedEventArgs args)
+    
+    private void ImageChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
-        foreach(ARTrackedImage trackedImage in args.added)
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
         {
             if (!_trackedImg.Contains(trackedImage))
             {
@@ -80,8 +79,8 @@ public class ImageTracker : MonoBehaviour
                 _trackedTimer.Add(0);
             }
         }
-
-        foreach(ARTrackedImage trackedImage in args.updated)
+        
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
         {
             if (!_trackedImg.Contains(trackedImage))
             {
@@ -91,18 +90,35 @@ public class ImageTracker : MonoBehaviour
             else
             {
                 int num = _trackedImg.IndexOf(trackedImage);
-                _trackedTimer[num] = 0;
+
+                // Limited 상태가 아닐때만 타이머를 0으로 만듬
+                if (_trackedImg[num].trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Limited)
+                {
+                }
+                else
+                {
+                    _trackedTimer[num] = 0;
+                }
             }
             UpdateImage(trackedImage);
         }
     }
-
+    
     private void UpdateImage(ARTrackedImage trackedImage)
     {
+        int num = _trackedImg.IndexOf(trackedImage);
         string name = trackedImage.referenceImage.name;
-        GameObject tObj = prefabDic[name];
-        tObj.transform.position = trackedImage.transform.position;
-        tObj.transform.rotation = trackedImage.transform.rotation;
-        tObj.SetActive(true);
+        GameObject tObj = _prefabDic[name];
+
+        // Limited 상태가 아닐때 위치 갱신, 활성화
+        if (_trackedImg[num].trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Limited)
+        {
+        }
+        else
+        {
+            tObj.transform.position = trackedImage.transform.position;
+            tObj.transform.rotation = trackedImage.transform.rotation;
+            tObj.SetActive(true);
+        }
     }
 }
